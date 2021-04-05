49522029842dffc7a24ec7f23d4e9d28bb9a87a7
heejung-gjt <hee_9803@naver.com>
2021-04-05 20:34:23 +0900

    feat: Add html,css
    TODO
    - Add detail section markup,styling
    - Add upload modal styling
    - Add scroll function

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
 const $mainItems = document.querySelector('.main-items');
 const $main = document.querySelector('.main');
 const $liContent = document.querySelector('.li-content');
-const $prc = document.querySelector('.prc')
-const $prcBtn = document.querySelector('.prc-btn');
-
+const $detail = document.querySelector('.detail')
+const $detailBtn = document.querySelector('.detail-btn');
 
 const removeUl = () => {
-    $prc.scrollIntoView({
-        block:'start',
-        behavior:'smooth',
-    });
-    $mainItems.style.display='none';
-
+  $detail.scrollIntoView({
+    block:'start',
+    behavior:'smooth',
+  });
+  setTimeout(function(){
+    $main.style.display='none';
+    $detailBtn.style.opacity='1';
+  },1000)
 }
-
 $mainItems.onclick = e => {
-    if(e.target.classList.contains('far')){
-        e.target.parentNode.style.display="none";
-    };
-    if(e.target.classList.contains('img')){
-        $prc.style.display='block';
-        removeUl();
-    }
+  if(e.target.classList.contains('far')){
+      e.target.parentNode.style.display="none";
+  };
+  if(e.target.classList.contains('img')){
+      $detail.style.display='block';
+      removeUl();
+  }
+}
+$detailBtn.onclick = () => {
+  $detailBtn.style.opacity='0';
+  $main.style.display='flex'; 
+  $main.scrollIntoView({
+    block:'end',
+    behavior:'smooth',
+  })
+  setTimeout(function(){
+    $detail.style.display='none';
+  },1000)
 }
 
-$prcBtn.onclick = () => {
-    $mainItems.style.display='flex';
-    window.scroll({
-        top:0,
-        behavior:'smooth'
-    })
-    setTimeout(function(){
-        $prc.style.display='none';
-    },1000)
-}
