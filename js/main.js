cd210132ece4094d2998090c7e1738608e576e3d
JuHyeong-K <wngud4680@gmail.com>
2021-04-06 15:58:59 +0900

    feat: Add edited date markup, styling
    TODO
    - Add edited date markup, stying
    - Refactor JS codes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const $main = document.querySelector('.main');
 const $liContent = document.querySelector('.li-content');
 const $detail = document.querySelector('.detail')
 const $detailBtn = document.querySelector('.detail-btn');
+const $detailContent = document.querySelector('.detail-content');
 // 등록버튼, 등록 모달 창 레이어
 const $mainSubmit = document.querySelector('.main-submit');
 const $modalUploadLayer = document.querySelector('.modal-upload-layer');
@@ -13,34 +14,40 @@ const $modalUpload = document.querySelector('.modal-upload');
 const $uploadAddButton = document.querySelector('.upload-add-button');
 const $uploadInputTitle = document.querySelector('.upload-input-title');
 const $uploadTextarea = document.querySelector('.upload-textarea');
+let todos = [];
 // 렌더링
 const render = () => {
-  $mainItems.innerHTML = todos.map(({id, title, date}) => 
+  $mainItems.innerHTML = todos.map(({id, title, date}) =>
   `<li class="li-item li-item${id}">
   <img src="img/메인.png" alt="item이미지" class="img">
-  <span class="far fa-times-circle"></span>
+  <span class="far fa-times-circle item-close"></span>
   <span class="li-date">${date}</span>
   <span class="li-title">${title}</span>
 </li>`
   )
 }
-const renderDetail = () => {
-  $mainItems.innerHTML = todos.map(({id, title, content, date}) => 
-  `<li class="li-item li-item${id}">
-  <img src="img/메인.png" alt="item이미지" class="img">
-  <span class="far fa-times-circle"></span>
-  <span class="li-date">${date}</span>
-  <span class="li-title">${title}</span>
-  <span class="li-detail">${content}</span>
-</li>`
-  )
-}
+const renderDetail = ({title, content, date, edited}) => {
+  $detailContent.innerHTML =
+  `<li class=ail-content-title">
+  <label for="de-title" class="a11y-hidden">제목</label>
+  <input class="detail-input-title" type="text" placeholder="제목">
+  <span class="detail-span-title">${title}</span>
+  <span class="detail-span-date"><span class="far fa-calendar"></span>posted
+    on<span class="date-underline">${date}</span></span><span class="detail-span-edited"><span
+      class="far fa-calendar-check"></span>Edited on<span
+      class="date-underline">${edited}</span></span>
+  </li>
+  <li class="detail-content-detail">
+    <textarea name="detail-textarea" class="detail-textarea" cols="30" rows="10" placeholder="내용"></textarea>
+    <span class="detail-span-textarea">${content}</span>
+  </li>`
+  )}
 const getTodos = () => {
   todos = [
-      {id: 4, title: "하루종일 공부한날.. 머리가 아푸다 흑흑", content: "내용4", date: "2021-04-06"},
-      {id: 3, title: "하루종일 공부한날.. 머리가 아푸다 흑흑", content: "내용2", date: "2021-04-05"},
-      {id: 2, title: "하루종일 공부한날.. 머리가 아푸다 흑흑", content: "내용3", date: "2021-04-04"},
-      {id: 1, title: "이히리어차차차이히리 오예오예 어차차 머리가 아푸다 흑흑", content: "내용", date: "2021-04-03"},
+      {id: 4, title: "하루종일 공부한날.. 머리가 아푸다 흑흑", content: "내용4", date: "2021-04-06", edited: ""},
+      {id: 3, title: "하루종일 공부한날.. 머리가 아푸다 흑흑", content: "내용2", date: "2021-04-05", edited: ""},
+      {id: 2, title: "하루종일 공부한날.. 머리가 아푸다 흑흑", content: "내용3", date: "2021-04-04", edited: ""},
+      {id: 1, title: "이히리어차차차이히리 오예오예 어차차 머리가 아푸다 흑흑", content: "내용", date: "2021-04-03", edited: ""},
   ];
   render()
 }
@@ -96,10 +103,10 @@ const scrollDetail = () => {
     block:'start',
     behavior:'smooth',
   });
-  setTimeout(function(){
-    $main.style.display='none';
-    $detailBtn.style.opacity='1';
-  },1000)
+        setTimeout(function(){
+            $main.style.display='none';
+            $detailBtn.style.opacity='1';
+        },1000)
 }
 $mainItems.onclick = e => {
   if(e.target.classList.contains('far')){
@@ -108,12 +115,16 @@ $mainItems.onclick = e => {
   };
   if(e.target.classList.contains('img')){
       $detail.style.display='block';
-      scrollDetail();
+      const item = e.target.parentNode.classList[1].slice(7)
+      let todoDetail = todos.filter(todo => todo.id == +item);
+      console.log(todoDetail)
+      renderDetail(todoDetail);
+        scrollDetail();
   }
 }
 $detailBtn.onclick = () => {
   $detailBtn.style.opacity='0';
-  $main.style.display='flex'; 
+  $main.style.display='flex';
   $main.scrollIntoView({
     block:'end',
     behavior:'smooth',
